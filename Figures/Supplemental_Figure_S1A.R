5868c5d2e7d5022fa2a8e108c99e3a2dccbb1d29
Yunlong Ma <11418032@zju.edu.cn>
2025-05-16 16:33:38 -0400

    Update Supplemental_Figure_S1A.R

--- Figures/Supplemental_Figure_S1A.R
+++ Figures/Supplemental_Figure_S1A.R
@@ -1,20 +1,41 @@
+#2025-05-16
+
+setwd("/Users/mayunlong/Desktop/UPENN/00UPenn-Projects/02-Aimed_projects/05-devBrain-RBP-methods/00-Manuscript-scRBP-2024/01_Data_analysis/00-Figures/")
+
 
 # Load necessary libraries
 library(ggplot2)
+library(reshape2)
+library(ggrepel)
 
-# Create the data frame
-data <- data.frame(
-  Years = c(2006, 2011, 2013, 2013, 2014, 2015, 2016, 2017, 2020, 2022, 2023, 2023, 2023),
-  Database = c("AEDB", "RBPDB", "SpliceAid-F", "CISBP-RNA", "RBPmap", "DoRiNA3", "ATtRACT", 
-               "MotifMap-RNA", "oRNAment", "POSTAR3", "ENCORI", "RBP-Tar", "GEO-2024"),
-  Number = c(108, 91, 18, 153, 223, 638, 1195, 244, 340, 4374, 9937, 3497, 4744)
-)
 
-# Generate the bar plot
-ggplot(data, aes(x = reorder(Database, -Number), y = Number, fill = as.factor(Years))) +
-  geom_bar(stat = "identity") +
-  labs(title = "Number of RBPs in Various Databases", x = "Database", y = "Number") +
-  theme_minimal(base_size = 14) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  scale_fill_discrete(name = "Year") +
+data <- read.csv("Sup_Figure_S1A.csv")
+
+# convert data format
+data_melted <- melt(data, id.vars = c("Years", "Database"), 
+                    measure.vars = c("Clustered_motifs", "Unclustered_motifs"), 
+                    variable.name = "Motif_Type", 
+                    value.name = "Count")
+
+
+data_melted <- melt(data, id.vars = c("Years", "Database"), 
+                    measure.vars = c("Clustered_motifs", "Unclustered_motifs"), 
+                    variable.name = "Motif_Type", 
+                    value.name = "Count")
+
+# set color
+colors <- c("Clustered_motifs" = "#66c2a5", "Unclustered_motifs" = "#fc8d62")
+
+# plot
+ggplot(data_melted, aes(x = reorder(Database, Count), y = Count, fill = Motif_Type)) + 
+  geom_bar(stat = "identity") + 
+  #geom_text(aes(label = Count), position = position_stack(vjust = 0.5), size = 3, color = "black") +
+  labs(title = "Motif Collection by Database", 
+       x = "Motif Collection", 
+       y = "Number of Motifs") + 
+  theme_minimal(base_size = 14) + 
+  theme(axis.text.y = element_text(size = 12, hjust = 1)) + 
+  scale_fill_manual(values = colors, name = "Exclusiveness") + 
   coord_flip()
+
+
