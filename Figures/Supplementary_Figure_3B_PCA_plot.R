839c63d5008c73f5cf1f3fe6f6ec230173e7a522
Yunlong Ma <11418032@zju.edu.cn>
2026-04-07 23:53:47 -0400

    Update Supplementary_Figure_3B_PCA_plot.R

--- Figures/Supplementary_Figure_3B_PCA_plot.R
+++ Figures/Supplementary_Figure_3B_PCA_plot.R
@@ -19,3 +19,39 @@ ggplot(pca_df, aes(x = PC1, y = PC2, label = method)) +
        color = "Cluster", fill = "Cluster")
 # Close PDF output
 dev.off()
+
+
+##-----Alternative
+
+library(umap)
+library(ggplot2)
+# Ensure matrix is numeric type
+jaccard_df <- as.data.frame(jaccard_matrix)
+jaccard_df <- jaccard_df[order(rownames(jaccard_df)), order(colnames(jaccard_df))]
+# --- PCA ---
+pca <- prcomp(jaccard_df, scale. = TRUE)
+pca_df <- as.data.frame(pca$x)
+pca_df$method <- rownames(jaccard_df)
+pdf("module_construction_PCA_15conditions.pdf", width = 6, height = 6)
+ggplot(pca_df, aes(x = PC1, y = PC2, label = method)) +
+  geom_point(size = 3) +
+  geom_text(size = 2, vjust = -0.8, check_overlap = TRUE) +
+  theme_minimal() +
+  labs(title = "PCA of Method Similarity (Jaccard)")
+# Close PDF output
+dev.off()
+
+# --- UMAP ---
+set.seed(42)
+umap_result <- umap(jaccard_df)
+umap_df <- as.data.frame(umap_result$layout)
+colnames(umap_df) <- c("UMAP1", "UMAP2")
+umap_df$method <- rownames(jaccard_df)
+pdf("module_construction_UMAP_15conditions.pdf", width = 6, height = 6)
+ggplot(umap_df, aes(x = UMAP1, y = UMAP2, label = method)) +
+  geom_point(size = 3) +
+  geom_text(size = 2, vjust = -0.8, check_overlap = TRUE) +
+  theme_minimal() +
+  labs(title = "UMAP of Method Similarity (Jaccard)")
+# Close PDF output
+dev.off()
