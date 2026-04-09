e3235a19c93ccc26baa81dc8a8700235f766bc74
Yunlong Ma <11418032@zju.edu.cn>
2026-04-09 16:33:05 -0400

    Create Supplementary_Figure_8A.R

---
+++ Figures/Supplementary_Figure_8A.R
@@ -0,0 +1,3 @@
+
+
+For Supplementary Figure 8a, please refer to scripts for processing and visualizing in [here](https://github.com/mayunlong89/scRBP_reproduce/blob/main/Figures/Figure_2H_transcript_region_preference.md)
